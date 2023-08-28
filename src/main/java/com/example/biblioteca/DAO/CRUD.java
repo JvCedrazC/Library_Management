6fcc3cbe53c87dc88728aa4957f4e3958d5477fa
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-08-28 09:32:10 -0300

    Create CRUD.java
    Creating CRUD generic interface

---
+++ src/main/java/com/example/biblioteca/DAO/CRUD.java
@@ -0,0 +1,15 @@
+package com.example.biblioteca.DAO;
+import java.util.List;
+
+public interface CRUD <T>{
+    public T create(T obj);
+    //Read
+    public T findById(int id);
+    public List<T> findMany();
+    //update
+    public void update(T obj);
+    //delete
+    public void deleteById(int id);
+    public void deleteMany();
+
+}
