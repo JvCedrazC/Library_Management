771cb464775971a9f97818de79f7c22ab92e12fa
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-08-28 10:25:34 -0300

    Update CustomerDAOListImplements.java
    Implementing interface methods

--- src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAOListImplements.java
+++ src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAOListImplements.java
@@ -1,4 +1,37 @@
 package com.example.biblioteca.DAO.customerDAO;
 
-public class CustomerDAOListImplements {
+import com.example.biblioteca.Model.Customer;
+
+import java.util.List;
+
+public class CustomerDAOListImplements implements CustomerDAO{
+    @Override
+    public Customer create(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(Customer obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
 }
