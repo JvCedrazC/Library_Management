1240db998d1ca41490c10a451e3e7d7b4e934a45
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-17 00:06:57 -0300

    :sparkles:  Feat: DAO

--- src/main/java/com/example/biblioteca/DAO/DAO.java
+++ src/main/java/com/example/biblioteca/DAO/DAO.java
@@ -1,5 +1,15 @@
 package com.example.biblioteca.DAO;
 
+import com.example.biblioteca.DAO.customerDAO.CustomerDAO;
+import com.example.biblioteca.DAO.customerDAO.CustomerDAOListImplements;
+
 public class DAO {
+    public static CustomerDAO customerDAO;
+    public static CustomerDAO getCustomer(){
+        if (customerDAO == null){
+            customerDAO = new CustomerDAOListImplements();
+        }
+        return customerDAO;
+    }
 
 }
