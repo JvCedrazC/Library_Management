c15e1899a0fd8c1673852c197b38aba670715656
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-17 00:07:20 -0300

    Create Book.java

---
+++ src/main/java/com/example/biblioteca/Model/Book.java
@@ -0,0 +1,73 @@
+package com.example.biblioteca.Model;
+
+import java.util.Date;
+
+public class Book {
+    private int id;
+    private int category_id;
+    private int ISBN;
+    private String name;
+    private String author;
+    private String publisher;
+    private String category;
+
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+}
