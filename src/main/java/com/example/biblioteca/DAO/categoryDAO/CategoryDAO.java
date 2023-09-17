0234b8e4656f94cf39fd72a223c9e0bd4b29a4f4
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-16 23:55:47 -0300

    Create CategoryDAO.java
    Creating Category DAO interface

---
+++ src/main/java/com/example/biblioteca/DAO/categoryDAO/CategoryDAO.java
@@ -0,0 +1,19 @@
+package com.example.biblioteca.DAO.categoryDAO;
+
+import com.example.biblioteca.DAO.CRUD;
+import com.example.biblioteca.Model.Category;
+
+
+import java.util.List;
+
+public interface CategoryDAO extends CRUD<Category> {
+    public Category create(Category category);
+    public Category findById(int id);
+    public List<Category> findMany();
+    //update
+    public void update(Category obj);
+    //delete
+    public void deleteById(int id);
+    public void deleteMany();
+
+}
