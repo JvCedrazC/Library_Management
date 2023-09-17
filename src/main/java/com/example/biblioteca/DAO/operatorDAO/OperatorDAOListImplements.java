b7dc76122ef19b023866126df93f805140e85d7b
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-17 00:07:03 -0300

    Create OperatorDAOListImplements.java

---
+++ src/main/java/com/example/biblioteca/DAO/operatorDAO/OperatorDAOListImplements.java
@@ -0,0 +1,38 @@
+package com.example.biblioteca.DAO.operatorDAO;
+
+import com.example.biblioteca.Model.Operator;
+
+import java.util.List;
+
+public class OperatorDAOListImplements implements OperatorDAO{
+    private int id;
+    @Override
+    public Operator create(Operator operator) {
+        return null;
+    }
+
+    @Override
+    public Operator findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Operator> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(Operator obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+}
