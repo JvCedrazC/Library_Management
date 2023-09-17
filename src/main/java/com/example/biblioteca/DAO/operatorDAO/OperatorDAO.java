911d17dc66f09872d746e78e8258075bdda17802
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-17 00:07:09 -0300

    Create OperatorDAO.java

---
+++ src/main/java/com/example/biblioteca/DAO/operatorDAO/OperatorDAO.java
@@ -0,0 +1,16 @@
+package com.example.biblioteca.DAO.operatorDAO;
+
+import com.example.biblioteca.DAO.CRUD;
+import com.example.biblioteca.Model.Operator;
+
+import java.util.List;
+
+public interface OperatorDAO extends CRUD<Operator> {
+    public Operator create(Operator operator);
+    public Operator findById(int id);
+    public List<Operator> findMany();
+    public void update(Operator obj);
+    public void deleteById(int id);
+    public void deleteMany();
+
+}
