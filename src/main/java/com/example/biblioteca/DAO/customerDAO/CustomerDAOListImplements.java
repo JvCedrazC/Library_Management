ff2fe4bb758280e83eafe4f7176aa460e07090be
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-17 00:02:47 -0300

    .

--- src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAOListImplements.java
+++ src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAOListImplements.java
@@ -2,16 +2,31 @@ package com.example.biblioteca.DAO.customerDAO;
 
 import com.example.biblioteca.Model.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAOListImplements implements CustomerDAO{
+    private int NextId;
+    private ArrayList<Customer> CustomerList;
+    public CustomerDAOListImplements(){
+        this.NextId = 0;
+        this.CustomerList = new ArrayList<Customer>();
+    }
     @Override
     public Customer create(Customer customer) {
-        return null;
+        customer.setId(NextId);
+        NextId++;
+        this.CustomerList.add(customer);
+        return customer;
     }
 
     @Override
     public Customer findById(int id) {
+        for (Customer customer: this.CustomerList){
+            if (customer.getId() == id){
+                return customer;
+            }
+        }
         return null;
     }
 
