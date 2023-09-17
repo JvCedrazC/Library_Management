5380177e3994bb5bc6f41d2a764337a11891792d
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-16 23:56:14 -0300

    Create CategoryDAOListImplements.java
    Implements categoryDAO interface

---
+++ src/main/java/com/example/biblioteca/DAO/categoryDAO/CategoryDAOListImplements.java
@@ -0,0 +1,38 @@
+package com.example.biblioteca.DAO.categoryDAO;
+
+import com.example.biblioteca.Model.Category;
+
+import java.util.List;
+
+public class CategoryDAOListImplements implements CategoryDAO{
+
+    @Override
+    public Category create(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Category> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(Category obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+}
