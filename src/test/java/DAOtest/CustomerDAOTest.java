155653cb1e459b67d38e83b6cb3a4d1ead6152c3
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-26 21:56:21 -0300

    :test_tube: test: CustomerDAO test
    create new teste called 'create()' for Customer DAO

--- src/test/java/DAOtest/CustomerDAOTest.java
+++ src/test/java/DAOtest/CustomerDAOTest.java
@@ -1,5 +1,23 @@
 package DAOtest;
 
-public class CustomerDAOTest {
+import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.DAO.customerDAO.CustomerDAO;
+import com.example.biblioteca.Model.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerDAOTest {
+    @BeforeEach
+    void teardown(){
+        DAO.getCustomer().deleteMany();
+    }
+    @Test
+    void create(){
+        CustomerDAO customerDAO = DAO.getCustomer();
+        Customer customer1 = new Customer(0, "LeBron James");
+        Customer novo1 = DAO.customerDAO.create(customer1);
+        assertEquals(novo1, customer1);
+    }
 }
