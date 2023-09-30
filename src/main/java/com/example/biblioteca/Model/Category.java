097c813f96343643af2e98dd20f2120f7e2fdf24
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 17:14:54 -0300

    :sparkles: feat
    create getters, setters and addbook function

--- src/main/java/com/example/biblioteca/Model/Category.java
+++ src/main/java/com/example/biblioteca/Model/Category.java
@@ -1,10 +1,21 @@
 package com.example.biblioteca.Model;
-
 import java.util.ArrayList;
 
 public class Category {
     private String name;
     private ArrayList<Integer> ISBNlist;
 
+    public Category(String name, int i){
+        setName(name);
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void addBook(int ISBN){
+        ISBNlist.add(ISBN);
+    }
 }
