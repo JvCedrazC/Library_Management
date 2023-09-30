5ffc0ba3dffa0ee1c969525580e48ef4ba08e357
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 17:12:51 -0300

    :recycle: refactor: updating the book class

--- src/main/java/com/example/biblioteca/Model/Book.java
+++ src/main/java/com/example/biblioteca/Model/Book.java
@@ -9,7 +9,16 @@ public class Book {
     private String name;
     private String author;
     private String publisher;
-    private String category;
+    private int locked = 0;
+
+    public Book(int isbn, int category_id, String name, String author, String publisher){
+        setISBN(isbn);
+        setName(name);
+        setCategory_id(category_id);
+        setAuthor(author);
+        setPublisher(publisher);
+
+    }
 
 
     //getters and setters
@@ -21,9 +30,7 @@ public class Book {
         this.id = id;
     }
 
-    public int getCategory_id() {
-        return category_id;
-    }
+    public int getCategory_id() {return category_id;}
 
     public void setCategory_id(int category_id) {
         this.category_id = category_id;
@@ -40,34 +47,26 @@ public class Book {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public String getPublisher() {
         return publisher;
     }
-
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
 
-    public String getCategory() {
-        return category;
+    public void setLocked(int locked) {
+        this.locked = locked;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
+    public int getLocked() {
+        return locked;
     }
-
-
 }
