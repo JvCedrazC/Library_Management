d54a5b3ff18e77b3978a28dea413af3ff33ec80e
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 17:23:07 -0300

    Update Operator.java

--- src/main/java/com/example/biblioteca/Model/Operator.java
+++ src/main/java/com/example/biblioteca/Model/Operator.java
@@ -1,19 +1,33 @@
 package com.example.biblioteca.Model;
 
+import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.DAO.bookDAO.BookDAO;
+import com.example.biblioteca.DAO.customerDAO.CustomerDAO;
+
 public class Operator extends Person{
+    BookDAO bookDAO = DAO.getBook();
+    //constructor
     public Operator(int id, String name){
         super(id, name);
     }
+
     //methods
-    public void make_loan(int customerID){
+    public void make_loan(int customerID, int ISBN){
+        Book book = bookDAO.findBYISBN(ISBN);
+        int bookId = book.getId();
+        LibraryLoan newLoan = new LibraryLoan(customerID, bookId);
 
     }
-    public void newBook(int isbn, String BookName, String bookCategory){
-
+    public void newBook(int isbn, String BookName, String publisher, String author, int category_id){
+        Book book = new Book(isbn, category_id, BookName, author, publisher);
+        bookDAO.create(book);
     }
 
-    public void newCustomer(){
-
+    public Customer newCustomer(String name){
+        CustomerDAO customerDAO = DAO.getCustomer();
+        Customer customer = new Customer(-1, name);
+        Customer newCustomer = customerDAO.create(customer);
+        return newCustomer;
     }
 
 }
