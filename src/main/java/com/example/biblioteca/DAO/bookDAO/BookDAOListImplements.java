14e2d4f20093899ec8c003a2ad76a53c0415475e
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 17:00:06 -0300

    build: :heavy_plus_sign: :Create Book DAO list impementation
    Creating the DAO for books and this class

---
+++ src/main/java/com/example/biblioteca/DAO/bookDAO/BookDAOListImplements.java
@@ -0,0 +1,79 @@
+package com.example.biblioteca.DAO.bookDAO;
+
+import com.example.biblioteca.Model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDAOListImplements implements BookDAO{
+    private int nextID;
+    private ArrayList<Book> bookList;
+    public BookDAOListImplements(){
+        this.bookList = new ArrayList<Book>();
+        nextID = 0;
+    }
+
+    @Override
+    public Book create(Book book) {
+        book.setId(nextID);
+        nextID++;
+        this.bookList.add(book);
+        return book;
+    }
+
+    @Override
+    public Book findById(int id) {
+        for (Book book : this.bookList){
+            if (book.getId() == id){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Book findBYISBN(int ISBN) {
+        for (Book book : this.bookList){
+            if (book.getISBN() == ISBN){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Book> findMany() {
+        List<Book> listbook = new ArrayList<Book>();
+        for (Object o: this.bookList){
+            listbook.add((Book) o);
+        }
+        return listbook;
+    }
+
+    @Override
+    public void update(Book obj) {
+        for (int i = 0; i <bookList.size(); i++){
+            if (this.bookList.get(i).getId() == obj.getId()){
+                this.bookList.set(i, obj);
+            }
+        }
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+        for (int i = 0; i < bookList.size(); i++){
+            if (this.bookList.get(i).getId() == id){
+                this.bookList.remove(i);
+            }
+        }
+
+    }
+
+    @Override
+    public void deleteMany() {
+        this.bookList = new ArrayList<Book>();
+        this.nextID = 0;
+
+    }
+}
