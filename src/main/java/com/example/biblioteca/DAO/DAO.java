5de7742948629f16b3fdc237a7e0d6864e80eb93
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 17:06:28 -0300

    Feat: :sparkles: create getBook() method
    create method to manage the DAO

--- src/main/java/com/example/biblioteca/DAO/DAO.java
+++ src/main/java/com/example/biblioteca/DAO/DAO.java
@@ -1,15 +1,25 @@
 package com.example.biblioteca.DAO;
 
+import com.example.biblioteca.DAO.bookDAO.BookDAO;
+import com.example.biblioteca.DAO.bookDAO.BookDAOListImplements;
 import com.example.biblioteca.DAO.customerDAO.CustomerDAO;
 import com.example.biblioteca.DAO.customerDAO.CustomerDAOListImplements;
 
 public class DAO {
     public static CustomerDAO customerDAO;
+    public static BookDAO bookDAO;
     public static CustomerDAO getCustomer(){
         if (customerDAO == null){
             customerDAO = new CustomerDAOListImplements();
         }
         return customerDAO;
+    }
+
+    public static BookDAO getBook(){
+        if (bookDAO == null){
+            bookDAO = new BookDAOListImplements();
+        }
+        return bookDAO;
     }
 
 }
