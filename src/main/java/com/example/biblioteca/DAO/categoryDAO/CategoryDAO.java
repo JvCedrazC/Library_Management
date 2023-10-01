de66eb97efe7c2099f9878fb06a423abc27b722c
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-01 09:36:33 -0300

    :construction: erro na ao trocar métodos

--- src/main/java/com/example/biblioteca/DAO/categoryDAO/CategoryDAO.java
+++ src/main/java/com/example/biblioteca/DAO/categoryDAO/CategoryDAO.java
@@ -2,13 +2,14 @@ package com.example.biblioteca.DAO.categoryDAO;
 
 import com.example.biblioteca.DAO.CRUD;
 import com.example.biblioteca.Model.Category;
+import com.example.biblioteca.Model.LibraryLoan;
 
 
 import java.util.List;
 
 public interface CategoryDAO extends CRUD<Category> {
     public Category create(Category category);
-    public Category findById(int id);
+    public LibraryLoan findById(int id);
     public List<Category> findMany();
     //update
     public void update(Category obj);
