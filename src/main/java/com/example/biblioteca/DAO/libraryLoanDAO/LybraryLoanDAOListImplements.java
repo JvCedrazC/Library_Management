6763b6848f5502087665326b11072e6971dadd1e
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-01 09:39:26 -0300

    :sparkles: LibraryLoanDAO Implementation

---
+++ src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LybraryLoanDAOListImplements.java
@@ -0,0 +1,47 @@
+package com.example.biblioteca.DAO.libraryLoanDAO;
+
+import com.example.biblioteca.Model.LibraryLoan;
+
+import java.util.List;
+
+public class LybraryLoanDAOListImplements implements LibraryLoanDAO{
+    @Override
+    public LibraryLoanDAO create(LibraryLoanDAO obj) {
+        return null;
+    }
+
+    @Override
+    public void update(LibraryLoanDAO obj) {
+
+    }
+
+    @Override
+    public LibraryLoan create(LibraryLoan libraryLoan) {
+        return null;
+    }
+
+    @Override
+    public LibraryLoan findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<LibraryLoan> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(LibraryLoan obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+}
