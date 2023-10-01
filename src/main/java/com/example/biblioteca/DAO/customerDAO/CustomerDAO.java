bdfd7a66e73e0fa24015369d9ffeb3612f51a9da
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-01 09:37:57 -0300

    Update CustomerDAO.java

--- src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAO.java
+++ src/main/java/com/example/biblioteca/DAO/customerDAO/CustomerDAO.java
@@ -2,6 +2,8 @@ package com.example.biblioteca.DAO.customerDAO;
 
 import com.example.biblioteca.Model.Customer;
 import com.example.biblioteca.DAO.CRUD;
+import com.example.biblioteca.Model.LibraryLoan;
+
 import java.util.List;
 public interface CustomerDAO extends CRUD<Customer> {
     public Customer create(Customer customer);
