fa15011f8cf4762759012237e4aec7fe32dfea0b
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-09-30 21:13:10 -0300

    :recycle: returning an ArrayList  of books

--- src/main/java/com/example/biblioteca/DAO/bookDAO/BookDAO.java
+++ src/main/java/com/example/biblioteca/DAO/bookDAO/BookDAO.java
@@ -3,13 +3,14 @@ package com.example.biblioteca.DAO.bookDAO;
 import com.example.biblioteca.DAO.CRUD;
 import com.example.biblioteca.Model.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface BookDAO extends CRUD<Book> {
 
     public Book create(Book book);
     public Book findById(int id);
-    public Book findBYISBN(int ISBN);
+    public ArrayList<Book> findBYISBN(int ISBN);
     public List<Book> findMany();
     public void update(Book obj);
     public void deleteById(int id);
