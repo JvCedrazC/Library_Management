e28da7a48ad12a1aba805f7f7742ab93d9acb64c
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-01 23:16:35 -0300

    :sparkles: feat: creating class constructorr

--- src/main/java/com/example/biblioteca/Model/Booking.java
+++ src/main/java/com/example/biblioteca/Model/Booking.java
@@ -1,8 +1,23 @@
 package com.example.biblioteca.Model;
 
+import java.time.LocalDate;
+
 public class Booking {
     private int customerID;
     private int ISBN;
+    private LocalDate startdate;
+    private LocalDate finishdate;
+
+    public void setCustomerID(int customerID, int ISBN, String startDate){
+        this.customerID = customerID;
+        this.ISBN = ISBN;
+    }
+    public void startBooking(){
+
+
+    }
+
+
 
 
 
