780cacff6297cd0980597fd09635ac049eb08b71
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 15:58:46 -0300

    :sparkles: feat: CRUD operations
    CRUD operations:
    -update(): Line26
    -findById(): Line 35
    -findMany(): Line 45
    -deleteById(): Line 54
    -deleteMany: line 64

--- src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LybraryLoanDAOListImplements.java
+++ src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LybraryLoanDAOListImplements.java
@@ -23,26 +23,45 @@ public class LybraryLoanDAOListImplements implements LibraryLoanDAO{
 
     @Override
     public void update(LibraryLoan libraryloan) {
-
+        for (int i = 0; i < this.libraryLoanList.size(); i++){
+            if (this.libraryLoanList.get(i).getLibraryLoanId() == libraryloan.getLibraryLoanId()){
+                this.libraryLoanList.set(i, libraryloan);
+            }
+        }
     }
 
     @Override
     public LibraryLoan findById(int id) {
+        for (LibraryLoan i: this.libraryLoanList){
+            if (i.getLibraryLoanId() == id){
+                return i;
+            }
+        }
         return null;
     }
 
     @Override
     public List<LibraryLoan> findMany() {
-        return null;
+        ArrayList<LibraryLoan> listLibraryLoan = new ArrayList<>();
+        for (Object o: this.libraryLoanList){
+            listLibraryLoan.add((LibraryLoan) o);
+        }
+        return listLibraryLoan;
     }
 
     @Override
     public void deleteById(int id) {
-
+        for (int i = 0; i < this.libraryLoanList.size();i++){
+            if (this.libraryLoanList.get(i).getLibraryLoanId() == id){
+                this.libraryLoanList.remove(i);
+                break;
+            }
+        }
     }
 
     @Override
     public void deleteMany() {
-
+        this.libraryLoanList = new ArrayList<>();
+        this.nextId = 0;
     }
 }
