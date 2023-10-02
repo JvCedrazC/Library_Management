ae440ea1f49c8116983019a20e0d6fb2d9fb591a
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-01 23:14:25 -0300

    :white_check_mark: Test: adding tests

--- src/test/java/DAOtest/CustomerDAOTest.java
+++ src/test/java/DAOtest/CustomerDAOTest.java
@@ -3,21 +3,67 @@ package DAOtest;
 import com.example.biblioteca.DAO.DAO;
 import com.example.biblioteca.DAO.customerDAO.CustomerDAO;
 import com.example.biblioteca.Model.Customer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerDAOTest {
-    @BeforeEach
+    CustomerDAO customerDAO = DAO.getCustomer();
+    @AfterEach
     void teardown(){
         DAO.getCustomer().deleteMany();
     }
+    @BeforeEach
+    void generate(){
+        Customer customer1 = new Customer(-1, "LeBron James");
+        customerDAO.create(customer1);
+        Customer customer2 = new Customer(-1, "Lionel Messi");
+        customerDAO.create(customer2);
+        Customer customer3 = new Customer(-1, "Cristiano Ronaldo");
+        customerDAO.create(customer3);
+    }
+
     @Test
     void create(){
-        CustomerDAO customerDAO = DAO.getCustomer();
         Customer customer1 = new Customer(0, "LeBron James");
         Customer novo1 = DAO.customerDAO.create(customer1);
         assertEquals(novo1, customer1);
+    }
+
+    @Test
+    void findByID(){
+        Customer customer = customerDAO.findById(1);
+        assertEquals("Lionel Messi", customer.getNome());
+    }
+
+    @Test
+    void findMany(){
+        ArrayList<Customer> customersList = new ArrayList<>();
+        customersList = (ArrayList<Customer>) DAO.getCustomer().findMany();
+        assertEquals(3, customersList.size());
+    }
+
+    @Test
+    void update(){
+        Customer newCustomer = customerDAO.findById(2);
+        newCustomer.setNome("Robert Lewandowski");
+        customerDAO.update(newCustomer);
+        assertEquals("Robert Lewandowski", customerDAO.findById(2).getNome());
+    }
+    @Test
+    void deleteById(){
+        customerDAO.deleteById(2);
+        assertEquals(2, customerDAO.findMany().size());
+    }
+
+    @Test
+    void deleteMany(){
+        customerDAO.deleteMany();
+        assertEquals(0, customerDAO.findMany().size()
+        );
     }
 }
