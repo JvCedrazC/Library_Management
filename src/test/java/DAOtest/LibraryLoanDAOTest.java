ed40722e70fb5078e21b06f394afc9f62ce2be3f
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 16:19:22 -0300

    :white_check_mark: test: Testing CRUD operations
    -create(): line 17

--- src/test/java/DAOtest/LibraryLoanDAOTest.java
+++ src/test/java/DAOtest/LibraryLoanDAOTest.java
@@ -1,4 +1,22 @@
 package DAOtest;
 
+import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.Model.LibraryLoan;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LibraryLoanDAOTest {
+    @AfterEach
+    void tearDown(){
+        DAO.getOperatorDAO().deleteMany();
+    }
+
+    @Test
+    public void create(){
+        LibraryLoan libraryLoan = new LibraryLoan(0, 978);
+        LibraryLoan newLibraryLoan = DAO.getLibraryLoanDAO().create(libraryLoan);
+        assertEquals(libraryLoan, newLibraryLoan);
+    }
 }
