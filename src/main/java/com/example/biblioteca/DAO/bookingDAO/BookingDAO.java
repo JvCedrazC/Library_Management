90d2900d9629b6f2fb0eacb028fbc7759f43dccb
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 17:56:26 -0300

    :construction: feat: new interface for BookingDAO

---
+++ src/main/java/com/example/biblioteca/DAO/bookingDAO/BookingDAO.java
@@ -0,0 +1,8 @@
+package com.example.biblioteca.DAO.bookingDAO;
+
+import com.example.biblioteca.DAO.CRUD;
+import com.example.biblioteca.Model.Booking;
+
+public interface BookingDAO extends CRUD<Booking> {
+
+}
