b29d4009d9240fc36bbdd4d28a667fd9c4fdb92d
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 15:10:39 -0300

    :construction: feat: creating constructor

--- src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LybraryLoanDAOListImplements.java
+++ src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LybraryLoanDAOListImplements.java
@@ -2,9 +2,17 @@ package com.example.biblioteca.DAO.libraryLoanDAO;
 
 import com.example.biblioteca.Model.LibraryLoan;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LybraryLoanDAOListImplements implements LibraryLoanDAO{
+    private ArrayList<LibraryLoan> libraryLoanList;
+    private int nextId;
+
+    public LybraryLoanDAOListImplements(){
+        this.libraryLoanList = new ArrayList<>();
+        this.nextId++;
+    }
     @Override
     public LibraryLoan create(LibraryLoan obj) {
         return null;
