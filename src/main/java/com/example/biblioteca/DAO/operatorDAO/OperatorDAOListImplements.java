309340e27d7ce1c4fe8f78f3adbafde4c86f3cca
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 10:53:54 -0300

    :sparkles: feat: method create (CRUD operations)

--- src/main/java/com/example/biblioteca/DAO/operatorDAO/OperatorDAOListImplements.java
+++ src/main/java/com/example/biblioteca/DAO/operatorDAO/OperatorDAOListImplements.java
@@ -3,20 +3,30 @@ package com.example.biblioteca.DAO.operatorDAO;
 import com.example.biblioteca.Model.LibraryLoan;
 import com.example.biblioteca.Model.Operator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OperatorDAOListImplements implements OperatorDAO{
-    private int id;
+    private int nextId;
+    private ArrayList<Operator> operatorList;
+
+    public OperatorDAOListImplements(){
+        this.nextId = 0;
+        this.operatorList = new ArrayList<>();
+
+    }
     @Override
     public Operator create(Operator operator) {
-        return null;
+        operator.setId(nextId);
+        this.operatorList.add(operator);
+        nextId++;
+        return operator;
     }
 
     @Override
     public Operator findById(int id) {
         return null;
     }
-
     @Override
     public List<Operator> findMany() {
         return null;
