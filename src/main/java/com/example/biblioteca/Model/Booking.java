e24b5f5c19c3d23c1231bd866e977426c8b518a3
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 01:43:57 -0300

    :sparkles: feat: adding new methods

--- src/main/java/com/example/biblioteca/Model/Booking.java
+++ src/main/java/com/example/biblioteca/Model/Booking.java
@@ -1,6 +1,7 @@
 package com.example.biblioteca.Model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Booking {
     private int customerID;
@@ -8,9 +9,11 @@ public class Booking {
     private LocalDate startdate;
     private LocalDate finishdate;
 
-    public void Booking(int customerID, int ISBN, String startDate){
+
+    public void Booking(int customerID, int ISBN, String startDate, int nDays){
         setCustomerID(customerID);
         setISBN(ISBN);
+        startBooking(startDate, nDays);
     }
     public void setISBN(int ISBN){
         this.ISBN = ISBN;
@@ -19,13 +22,35 @@ public class Booking {
         this.customerID = customerID;
     }
 
-    public void startBooking(String date){
-            
-
+    public void setStartdate(LocalDate startdate) {
+        this.startdate = startdate;
     }
 
+    public void setFinishdate(LocalDate finishdate) {
+        this.finishdate = finishdate;
+    }
 
+    public int getCustomerID() {
+        return customerID;
+    }
 
+    public int getISBN() {
+        return ISBN;
+    }
 
+    public LocalDate getStartdate() {
+        return startdate;
+    }
+
+    public LocalDate getFinishdate() {
+        return finishdate;
+    }
+
+    public void startBooking(String date, int nDays){
+        LocalDate startDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate finishDate = startDate.plusDays(nDays);
+        setStartdate(startDate);
+        setFinishdate(finishDate);
+    }
 
 }
