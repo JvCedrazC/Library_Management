cebe8641724d77b9b4aadcedafdb64ef319958ea
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 17:51:09 -0300

    :white_check_mark: test: CRUD operations
    findById(): Line 47

---
+++ src/main/java/com/example/biblioteca/DAO/admDAO/AdmDAOListImplements.java
@@ -0,0 +1,38 @@
+package com.example.biblioteca.DAO.admDAO;
+
+import com.example.biblioteca.Model.Adm;
+
+import java.util.List;
+
+public class AdmDAOListImplements implements AdmDAO{
+
+    @Override
+    public Adm create(Adm obj) {
+        return null;
+    }
+
+    @Override
+    public Adm findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Adm> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(Adm obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+}
--- src/test/java/DAOtest/LibraryLoanDAOTest.java
+++ src/test/java/DAOtest/LibraryLoanDAOTest.java
@@ -1,9 +1,13 @@
 package DAOtest;
 
 import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.Model.Book;
 import com.example.biblioteca.Model.LibraryLoan;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,6 +15,26 @@ public class LibraryLoanDAOTest {
     @AfterEach
     void tearDown(){
         DAO.getOperatorDAO().deleteMany();
+        DAO.getBook().deleteMany();
+    }
+    @BeforeEach
+    void generateLibraryLoan(){
+        LibraryLoan libraryLoan1 = new LibraryLoan(0, 978);
+        DAO.getLibraryLoanDAO().create(libraryLoan1);
+        LibraryLoan libraryLoan2 = new LibraryLoan(1, 978);
+        DAO.getLibraryLoanDAO().create(libraryLoan2);
+        LibraryLoan libraryLoan3 = new LibraryLoan(2, 7117);
+        DAO.getLibraryLoanDAO().create(libraryLoan3);
+
+    }
+    @BeforeEach
+    void generateBooks(){
+        Book book1 = new Book(978, 0, "1808", "Laurentino Gomes", "Planeta Jovem");
+        DAO.getBook().create(book1);
+        Book book2 = new Book(978, 0, "1808", "Laurentino Gomes", "Planeta Jovem");
+        DAO.getBook().create(book2);
+        Book book3 = new Book(7117, 1, "Como Treinar seu Dragão", "Cressida Cowell", "Intrinseca");
+        DAO.getBook().create(book3);
     }
 
     @Test
@@ -19,4 +43,10 @@ public class LibraryLoanDAOTest {
         LibraryLoan newLibraryLoan = DAO.getLibraryLoanDAO().create(libraryLoan);
         assertEquals(libraryLoan, newLibraryLoan);
     }
+    @Test
+    public void findById(){
+        LibraryLoan libraryLoan1 = DAO.getLibraryLoanDAO().findById(1);
+        assertEquals(libraryLoan1.getBookISBN(), 978);
+    }
+    
 }
