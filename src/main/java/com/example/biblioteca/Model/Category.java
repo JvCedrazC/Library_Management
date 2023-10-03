13b36058c7b0f04d91653df620a5183d81c6c535
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 23:59:30 -0300

    Update Category.java

--- src/main/java/com/example/biblioteca/Model/Category.java
+++ src/main/java/com/example/biblioteca/Model/Category.java
@@ -14,6 +14,15 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
     public String getName(){
         return this.name;
     }
