c7c7cd9c2546dced765d7a0a98efd0ec6a04ee91
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 22:58:08 -0300

    Create OperatorTest.java

---
+++ src/test/java/ModelTest/OperatorTest.java
@@ -0,0 +1,21 @@
+package ModelTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OperatorTest {
+    @BeforeEach
+    void generate(){
+
+    }
+
+    @AfterEach
+    void teardown(){
+
+    }
+    @Test
+    void makeLoan(){
+
+    }
+}
