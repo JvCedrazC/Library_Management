474f5189ed8ef0824c13088ecaed4a031da1c522
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-02 22:58:12 -0300

    Create LibraryLoanTest.java

---
+++ src/test/java/ModelTest/LibraryLoanTest.java
@@ -0,0 +1,10 @@
+package ModelTest;
+
+import com.example.biblioteca.Model.LibraryLoan;
+
+public class LibraryLoanTest {
+    void teste (){
+        LibraryLoan libraryLoan = new LibraryLoan(0, 978);
+        libraryLoan.
+    }
+}
