c12e539df3fef86001f6cb4e725f0ad3b45499ac
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-05 10:37:47 -0300

    :construction: test: Create the class suiteTest

---
+++ src/main/java/com/example/biblioteca/DAO/bookingDAO/BookingDAOListImplements.java
@@ -0,0 +1,37 @@
+package com.example.biblioteca.DAO.bookingDAO;
+
+import com.example.biblioteca.Model.Booking;
+
+import java.util.List;
+
+public class BookingDAOListImplements implements BookingDAO{
+    @Override
+    public Booking create(Booking obj) {
+        return null;
+    }
+
+    @Override
+    public Booking findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> findMany() {
+        return null;
+    }
+
+    @Override
+    public void update(Booking obj) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+}
