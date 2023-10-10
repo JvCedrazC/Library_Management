da01db341396e5a6849f69040c8cbe2c2dd1cd07
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-10-10 10:48:56 -0300

    :white_check_mark: test: method genrate and tearDown in BookingDAO test

--- src/test/java/DAOtest/BookingDAOTest.java
+++ src/test/java/DAOtest/BookingDAOTest.java
@@ -1,8 +1,10 @@
 package DAOtest;
 
 import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.Model.Book;
 import com.example.biblioteca.Model.Booking;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +13,17 @@ public class BookingDAOTest {
     @AfterEach
     void tearDown(){
         DAO.getBookingDAO().deleteMany();
+        DAO.getBook().deleteMany();
+    }
+
+    @BeforeEach
+    void generate(){
+        Book book1 = new Book(978, 0, "1808", "Laurentino Gomes", "Planeta Jovem");
+        DAO.getBook().create(book1);
+        Book book2 = new Book(978, 0, "1808", "Laurentino Gomes", "Planeta Jovem");
+        DAO.getBook().create(book2);
+        Book book3 = new Book(7117, 1, "Como Treinar seu Dragão", "Cressida Cowell", "Intrinseca");
+        DAO.getBook().create(book3);
     }
 
     @Test
@@ -19,5 +32,8 @@ public class BookingDAOTest {
         Booking newBooking = DAO.getBookingDAO().create(booking);
         assertEquals(booking, newBooking);
     }
+
+
+
 
 }
