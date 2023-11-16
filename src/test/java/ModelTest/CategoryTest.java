ddb083206afa99e0714e1773395708519589776f
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-11-16 12:03:05 -0300

    Update CategoryTest.java

--- src/test/java/ModelTest/CategoryTest.java
+++ src/test/java/ModelTest/CategoryTest.java
@@ -1,10 +1,18 @@
 package ModelTest;
 
+import com.example.biblioteca.DAO.DAO;
+import com.example.biblioteca.Model.Book;
+import com.example.biblioteca.Model.Category;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CategoryTest {
     @Test
     public void addBook(){
-
+        Book book1 = new Book(978, 0, "1808", "Laurentino Gomes", "Planeta Jovem");
+        Category category = new Category("History", 978);
+        Category ncategory = DAO.getCategoryDAO().create(category);
+        assertEquals(1, ncategory.getISBNlist().size());
     }
 }
