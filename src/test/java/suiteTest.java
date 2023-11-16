bb48c75b8fa6f6e1029ff6429842a3d86fc36653
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-11-16 12:00:54 -0300

    :white_check_mark: test: implments suitTest

--- src/test/java/suiteTest.java
+++ src/test/java/suiteTest.java
@@ -1,2 +1,16 @@
-public class suiteTest {
-}
+import DAOtest.*;
+import com.example.biblioteca.DAO.operatorDAO.OperatorDAO;
+import org.junit.runner.*;
+import org.junit.runners.*;
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {
+        AdmDAOTest.class,
+        BookingDAOTest.class,
+        BookDAOTest.class,
+        //CategoryDAOTest.class,
+        CustomerDAOTest.class,
+        LibraryLoanDAOTest.class,
+        TestOperatorDAO.class
+
+})
+public class suiteTest {}
