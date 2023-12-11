a2a9fd56025466898fb00a767b2225a62e744f87
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-12-10 23:49:31 -0300

    :construction: feat: Constructor of ManagingFiles class

---
+++ src/main/java/com/example/biblioteca/FileUtils/ManagingFiles.java
@@ -0,0 +1,13 @@
+package com.example.biblioteca.FileUtils;
+
+import java.io.Serializable;
+
+public class ManagingFiles implements Serializable {
+    private String fileName;
+
+    public ManagingFiles(String fileName){
+        this.fileName = fileName;
+    }
+    
+
+}
