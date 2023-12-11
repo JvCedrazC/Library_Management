c4a31007e7671b9853b46ef2d96573a1a120dcb4
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-12-11 14:13:09 -0300

    :sparkles: feat: Implementing Method to Save and Retrive from files

--- src/main/java/com/example/biblioteca/FileUtils/ManagingFiles.java
+++ src/main/java/com/example/biblioteca/FileUtils/ManagingFiles.java
@@ -1,6 +1,7 @@
 package com.example.biblioteca.FileUtils;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ManagingFiles implements Serializable {
     private String fileName;
@@ -8,6 +9,34 @@ public class ManagingFiles implements Serializable {
     public ManagingFiles(String fileName){
         this.fileName = fileName;
     }
-    
+
+    public <T> void save(ArrayList<T> list){
+        try {
+            FileOutputStream file = new FileOutputStream(this.fileName);
+            ObjectOutputStream object = new ObjectOutputStream(file);
+            object.writeObject(list);
+            object.close();
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public <T> ArrayList<T> retrieve(){
+        try{
+            ArrayList<T> list;
+            File file = new File(fileName);
+            if (!file.exists()){
+                save(new ArrayList<T>());
+            }
+            FileInputStream fileStream = new FileInputStream(this.fileName);
+            ObjectInputStream os = new ObjectInputStream(fileStream);
+            list = (ArrayList<T>) os.readObject();
+            os.close();
+            return list;
+        }catch (IOException | ClassNotFoundException exception){
+            throw new RuntimeException(exception);
+        }
+
+    }
 
 }
