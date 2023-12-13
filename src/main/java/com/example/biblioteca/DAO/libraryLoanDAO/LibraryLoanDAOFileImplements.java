ef966b94ee3ecec546f4d9eb36e2938d0671461b
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-12-12 21:30:06 -0300

    :sparkles: feat: Implementing DAO with files for Customers

---
+++ src/main/java/com/example/biblioteca/DAO/libraryLoanDAO/LibraryLoanDAOFileImplements.java
@@ -0,0 +1,67 @@
+package com.example.biblioteca.DAO.libraryLoanDAO;
+
+import com.example.biblioteca.Model.LibraryLoan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryLoanDAOFileImplements implements LibraryLoanDAO {
+    private ArrayList<LibraryLoan> libraryLoanList;
+    private int nextId;
+
+    public LibraryLoanDAOFileImplements(){
+        this.libraryLoanList = new ArrayList<>();
+        this.nextId++;
+    }
+    @Override
+    public LibraryLoan create(LibraryLoan obj) {
+        obj.setLibraryLoanId(nextId);
+        this.libraryLoanList.add(obj);
+        nextId++;
+        return obj;
+    }
+
+    @Override
+    public void update(LibraryLoan libraryloan) {
+        for (int i = 0; i < this.libraryLoanList.size(); i++){
+            if (this.libraryLoanList.get(i).getLibraryLoanId() == libraryloan.getLibraryLoanId()){
+                this.libraryLoanList.set(i, libraryloan);
+            }
+        }
+    }
+
+    @Override
+    public LibraryLoan findById(int id) {
+        for (LibraryLoan i: this.libraryLoanList){
+            if (i.getLibraryLoanId() == id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<LibraryLoan> findMany() {
+        ArrayList<LibraryLoan> listLibraryLoan = new ArrayList<>();
+        for (Object o: this.libraryLoanList){
+            listLibraryLoan.add((LibraryLoan) o);
+        }
+        return listLibraryLoan;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        for (int i = 0; i < this.libraryLoanList.size();i++){
+            if (this.libraryLoanList.get(i).getLibraryLoanId() == id){
+                this.libraryLoanList.remove(i);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void deleteMany() {
+        this.libraryLoanList = new ArrayList<>();
+        this.nextId = 0;
+    }
+}
