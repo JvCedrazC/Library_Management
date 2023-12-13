c99a6ea1361790aef24ef23794fe3afc19d35356
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-12-12 22:53:40 -0300

    :bulb: docs: adding javadocs

--- src/main/java/com/example/biblioteca/FileUtils/ManagingFiles.java
+++ src/main/java/com/example/biblioteca/FileUtils/ManagingFiles.java
@@ -9,11 +9,21 @@ public class ManagingFiles implements Serializable {
      * Nome do arquivo utilizado para operações de leitura e escrita.
      */
     private String fileName;
-
+    /**
+     * Construtor da classe ManagingFiles que recebe o nome do arquivo.
+     *
+     * @param fileName O nome do arquivo a ser utilizado.
+     */
     public ManagingFiles(String fileName){
         this.fileName = fileName;
     }
 
+    /**
+     * Salva a lista de objetos no arquivo especificado.
+     *
+     * @param list A lista de objetos a ser salva no arquivo.
+     * @param <T> O tipo dos objetos na lista.
+     */
     public <T> void save(ArrayList<T> list){
         try {
             FileOutputStream file = new FileOutputStream(this.fileName);
@@ -25,6 +35,13 @@ public class ManagingFiles implements Serializable {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Recupera a lista de objetos do arquivo especificado.
+     *
+     * @return A lista de objetos recuperada do arquivo.
+     * @param <T> O tipo dos objetos na lista.
+     */
     public <T> ArrayList<T> retrieve(){
         try{
             ArrayList<T> list;
